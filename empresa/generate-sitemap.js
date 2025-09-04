bf6986f19ad42b1dfc2e66a606688d20f7619089
AlejandroMV03 <alejandromexxx894@gmail.com>
2025-09-04 16:27:12 -0600

    actualizado

---
+++ empresa/generate-sitemap.js
@@ -0,0 +1,33 @@
+// sitemap.js
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { writeFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Para obtener el directorio actual del archivo
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Archivo final
+const sitemapPath = path.join(__dirname, 'public', 'sitemap.xml');
+
+const hostname = 'https://creavixsfc.com';
+const sitemap = new SitemapStream({ hostname });
+
+// Rutas de tu proyecto
+const pages = [
+  '/',
+  '/Home',
+  '/Contacto',
+  '/Somos',
+];
+
+// Agregamos las rutas al sitemap
+pages.forEach((page) => sitemap.write({ url: page, changefreq: 'weekly', priority: 0.8 }));
+sitemap.end();
+
+// Generamos el sitemap y lo guardamos
+streamToPromise(sitemap)
+  .then((data) => writeFile(sitemapPath, data.toString()))
+  .then(() => console.log(`Sitemap generado en ${sitemapPath}`))
+  .catch((err) => console.error(err));
